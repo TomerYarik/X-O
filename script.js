7f10f151355c48f265c6fd27f943cd8bacd7d1a3
TomerYarik <yarinovsky.tomer@gmail.com>
2021-12-15 18:42:41 +0200

    game works, need th make functions cleaner

--- script.js
+++ script.js
@@ -4,25 +4,8 @@ let player = 0; //decides which player is current
 let cells = document.getElementsByClassName('cell'); //array of cells
 const memoryX = {}; // object to mark the positions of each X
 const memoryO = {}; //object to mark the positions of each O
-for(let i=0;i<cells.length; i++){
-    const currentCell = cells[i];
-    //switches the player and marks the cell as X or O
-    const switchPlayer = function() {
-        if(!currentCell.classList.contains(`selected`)) {
-            currentCell.classList.add('selected');
-            currentCell.classList.add(`selected${player}`);
-            currentCell.textContent = player === 0 ? 'X' : 'O';
-            player === 0 ? player++ : player--;
-        }
-        //pushes the Xs to the memoryX and the Os to memoryO
-        currentCell.textContent === 'X' ? memoryX[i] = currentCell.textContent : memoryO[i] = currentCell.textContent; 
-        const arrX = Object.keys(memoryX);
-        const arrO = Object.keys(memoryO);
-    }
-    //every time the player clicks it calls the switchPlayer function
-    currentCell.addEventListener('click', switchPlayer);
-}
-
+const resetBtn = document.querySelector('.resetBtn');
+const winner = document.querySelector('.winner');
 const checkWin = function(array) {
     if(array.includes('0') && array.includes('1') && array.includes('2')) return true;
     else if(array.includes('0') && array.includes('3') && array.includes('6')) return true;
@@ -36,3 +19,51 @@ const checkWin = function(array) {
         return false;
     }
 }
+
+function resetAll() {
+    for(let cell of cells) {
+        cell.classList.remove('selected');
+        cell.classList.remove('selected1') || cell.classList.remove('selected0');
+        cell.textContent = '';
+        player =0;
+        for(const key of Object.keys(memoryO)) delete memoryO[key];
+        for(const key of Object.keys(memoryX)) delete memoryX[key];
+        winner.textContent = '';
+    }
+    resetBtn.style.display = 'none';
+}
+
+function switchingPlayer(currentCell) {
+    if(!currentCell.classList.contains(`selected`)) {
+    currentCell.classList.add('selected');
+    currentCell.classList.add(`selected${player}`);
+    currentCell.textContent = player === 0 ? 'X' : 'O';
+    player === 0 ? player++ : player--;
+    }
+}
+
+for(let i=0;i<cells.length; i++){
+    const currentCell = cells[i];
+    const gameEvents = function() {
+        //switches the player and marks the cell as X or O
+        switchingPlayer(currentCell);
+        //pushes the Xs to the memoryX and the Os to memoryO
+        currentCell.textContent === 'X' ? memoryX[i] = currentCell.textContent : memoryO[i] = currentCell.textContent;
+        //makes arrays from the objects
+        const arrX = Object.keys(memoryX);
+        const arrO = Object.keys(memoryO);
+        if(checkWin(arrX) || checkWin(arrO)) {
+            for(const cell of cells) {
+                cell.classList.add('selected');
+            }
+            player === 0 ? player++ : player--;
+            resetBtn.style.display = 'inline';
+            winner.textContent = `player ${player + 1} won!`;
+        }
+        if(arrX.length === 5 && arrO.length === 4) resetBtn.style.display = 'inline';
+    }
+    //every time the player clicks it calls the gameEvents function
+    currentCell.addEventListener('click', gameEvents);
+}
+//reset button
+resetBtn.addEventListener('click', resetAll);
