e9959fcbf269d898bb380086a22a4b55b85d62e0
TomerYarik <yarinovsky.tomer@gmail.com>
2021-12-15 19:13:45 +0200

    game is done, one bug is that player switches after win or draw

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const memoryX = {}; // object to mark the positions of each X
 const memoryO = {}; //object to mark the positions of each O
 const resetBtn = document.querySelector('.resetBtn');
 const winner = document.querySelector('.winner');
+//functions
+
+//checks if the current array contains ine of the eight winning combos
 const checkWin = function(array) {
     if(array.includes('0') && array.includes('1') && array.includes('2')) return true;
     else if(array.includes('0') && array.includes('3') && array.includes('6')) return true;
@@ -20,6 +23,7 @@ const checkWin = function(array) {
     }
 }
 
+//resets the game
 function resetAll() {
     for(let cell of cells) {
         cell.classList.remove('selected');
@@ -32,13 +36,34 @@ function resetAll() {
     }
     resetBtn.style.display = 'none';
 }
-
+//selects the current cell and switches to the next player
 function switchingPlayer(currentCell) {
     if(!currentCell.classList.contains(`selected`)) {
     currentCell.classList.add('selected');
     currentCell.classList.add(`selected${player}`);
     currentCell.textContent = player === 0 ? 'X' : 'O';
-    player === 0 ? player++ : player--;
+    if(resetBtn.style.display = 'hidden') player === 0 ? player++ : player--;
+    }
+}
+
+//pushes the memory object keys to arrays
+function pushToArr(currentCell, i) {
+    currentCell.textContent === 'X' ? memoryX[i] = currentCell.textContent : memoryO[i] = currentCell.textContent;
+}
+
+//checks if someone won, and if so, declares who won, else ddeclares draw
+function declareWinner(arrX ,arrO) {
+    //checks if someone won
+    if(checkWin(arrO) || checkWin(arrX)) {
+        for(const cell of cells) cell.classList.add('selected');
+        if(resetBtn.style.display = ' hidden') player === 0 ? player++ : player--;
+        resetBtn.style.display = 'inline';
+        winner.textContent = `player ${player + 1} won!`;
+    }
+    //checks if no one won
+    else if(arrX.length === 5 && arrO.length === 4) {
+        resetBtn.style.display = 'inline';
+        winner.textContent = 'Draw!';
     }
 }
 
@@ -48,22 +73,16 @@ for(let i=0;i<cells.length; i++){
         //switches the player and marks the cell as X or O
         switchingPlayer(currentCell);
         //pushes the Xs to the memoryX and the Os to memoryO
-        currentCell.textContent === 'X' ? memoryX[i] = currentCell.textContent : memoryO[i] = currentCell.textContent;
+        pushToArr(currentCell, i);
         //makes arrays from the objects
         const arrX = Object.keys(memoryX);
         const arrO = Object.keys(memoryO);
-        if(checkWin(arrX) || checkWin(arrO)) {
-            for(const cell of cells) {
-                cell.classList.add('selected');
-            }
-            player === 0 ? player++ : player--;
-            resetBtn.style.display = 'inline';
-            winner.textContent = `player ${player + 1} won!`;
-        }
-        if(arrX.length === 5 && arrO.length === 4) resetBtn.style.display = 'inline';
+        //checks if someone won and if so, declares the winner
+        declareWinner(arrX, arrO);
     }
     //every time the player clicks it calls the gameEvents function
     currentCell.addEventListener('click', gameEvents);
 }
+
 //reset button
 resetBtn.addEventListener('click', resetAll);
