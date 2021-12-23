982ebff7436c90fd279a771094c6c0220ec97362
TomerYarik <yarinovsky.tomer@gmail.com>
2021-12-23 18:09:36 +0200

    started working on easy game mode

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 'use strict'
 
-let player = 0; //decides which player is current
+//selectors
+let player = 1; //decides which player is current
 let cells = document.getElementsByClassName('cell'); //array of cells
 const memoryX = {}; // object to mark the positions of each X
 const memoryO = {}; //object to mark the positions of each O
 const resetBtn = document.querySelector('.resetBtn');
 const winner = document.querySelector('.winner');
+const btnEasy = document.querySelector('.easy');
+const btnHard = document.querySelector('.hard');
+const btn1V1 = document.querySelector('.PVP');
+const gameContainer = document.querySelector('.game');
+const selectorContainer = document.querySelector('.difficulty');
+const btnPlayer = document.querySelector('.player');
+const btnPC = document.querySelector('.PC');
+
 //functions
 
 //checks if the current array contains ine of the eight winning combos
@@ -35,14 +44,15 @@ function resetAll() {
         winner.textContent = '';
     }
     resetBtn.style.display = 'none';
+    updateToStart();
 }
 //selects the current cell and switches to the next player
 function switchingPlayer(currentCell) {
     if(!currentCell.classList.contains(`selected`)) {
-    currentCell.classList.add('selected');
-    currentCell.classList.add(`selected${player}`);
-    currentCell.textContent = player === 0 ? 'X' : 'O';
-    if(resetBtn.style.display = 'hidden') player === 0 ? player++ : player--;
+        currentCell.classList.add('selected');
+        currentCell.classList.add(`selected${player}`);
+        currentCell.textContent = player === 0 ? 'X' : 'O';
+        if(resetBtn.style.display = 'hidden') player === 0 ? player++ : player--;
     }
 }
 
@@ -61,28 +71,50 @@ function declareWinner(arrX ,arrO) {
         winner.textContent = `player ${player + 1} won!`;
     }
     //checks if no one won
-    else if(arrX.length === 5 && arrO.length === 4) {
+    else if(arrX.length + arrO.length === 9) {
         resetBtn.style.display = 'inline';
         winner.textContent = 'Draw!';
     }
 }
+function updateGameDisplay() {
+    selectorContainer.style.display = "none";
+    gameContainer.style.display = "inline";
+}
+function updateToStart() {
+    selectorContainer.style.display = "inline";
+    gameContainer.style.display = "none";
+}
 
-for(let i=0;i<cells.length; i++){
-    const currentCell = cells[i];
-    const gameEvents = function() {
-        //switches the player and marks the cell as X or O
-        switchingPlayer(currentCell);
-        //pushes the Xs to the memoryX and the Os to memoryO
-        pushToArr(currentCell, i);
-        //makes arrays from the objects
-        const arrX = Object.keys(memoryX);
-        const arrO = Object.keys(memoryO);
-        //checks if someone won and if so, declares the winner
-        declareWinner(arrX, arrO);
+//sets the game to a 1V1 game
+function set1V1() {
+    updateGameDisplay();
+    for(let i=0;i<cells.length; i++){
+        const currentCell = cells[i];
+        const gameEvents = function() {
+            //switches the player and marks the cell as X or O
+            switchingPlayer(currentCell);
+            //pushes the Xs to the memoryX and the Os to memory
+            pushToArr(currentCell, i);
+            //makes arrays from the objects
+            const arrX = Object.keys(memoryX);
+            const arrO = Object.keys(memoryO);
+            //checks if someone won and if so, declares the winner
+            declareWinner(arrX, arrO);
+        }
+        //every time the player clicks it calls the gameEvents function
+        currentCell.addEventListener('click', gameEvents);
     }
-    //every time the player clicks it calls the gameEvents function
-    currentCell.addEventListener('click', gameEvents);
+}
+
+function setEasy() {
+
+}
+function setHard() {
+
 }
 
 //reset button
 resetBtn.addEventListener('click', resetAll);
+btnEasy.addEventListener('click', setEasy);
+btn1V1.addEventListener('click', set1V1);
+btnHard.addEventListener('click', setHard)
