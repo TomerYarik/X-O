f6aa8b728599024d0a6c3eb5b59fc0fec4b9e2a6
TomerYarik <yarinovsky.tomer@gmail.com>
2021-12-24 13:29:30 +0200

    buggy easy mode

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
 //selectors
-let player = 1; //decides which player is current
+let player; //decides which player is current
 let cells = document.getElementsByClassName('cell'); //array of cells
 const memoryX = {}; // object to mark the positions of each X
 const memoryO = {}; //object to mark the positions of each O
@@ -14,6 +14,10 @@ const gameContainer = document.querySelector('.game');
 const selectorContainer = document.querySelector('.difficulty');
 const btnPlayer = document.querySelector('.player');
 const btnPC = document.querySelector('.PC');
+const btnPlayer0 = document.querySelector('.btnPlayer0');
+const btnPlayer1 = document.querySelector('.btnPlayer1');
+const selectPlayerContainer = document.querySelector('.selectPlayerStarter');
+const selectPlayerOrPcContainer = document.querySelector('.selectPlayer');
 
 //functions
 
@@ -46,12 +50,21 @@ function resetAll() {
     resetBtn.style.display = 'none';
     updateToStart();
 }
+function clearMemory() {
+    for(const key in memoryX) delete memoryX[key];
+    for(const key in memoryO) delete memoryO[key];
+}
+
+function addSelectedClass(currentCell) {
+    currentCell.classList.add('selected');
+    currentCell.classList.add(`selected${player}`);
+    currentCell.textContent = player === 0 ? 'X' : 'O';
+
+}
 //selects the current cell and switches to the next player
 function switchingPlayer(currentCell) {
     if(!currentCell.classList.contains(`selected`)) {
-        currentCell.classList.add('selected');
-        currentCell.classList.add(`selected${player}`);
-        currentCell.textContent = player === 0 ? 'X' : 'O';
+        addSelectedClass(currentCell);
         if(resetBtn.style.display = 'hidden') player === 0 ? player++ : player--;
     }
 }
@@ -64,54 +77,108 @@ function pushToArr(currentCell, i) {
 //checks if someone won, and if so, declares who won, else ddeclares draw
 function declareWinner(arrX ,arrO) {
     //checks if someone won
-    if(checkWin(arrO) || checkWin(arrX)) {
+    if(checkWin(arrO)) {
         for(const cell of cells) cell.classList.add('selected');
-        checkWin(arrX) ? player = 0 : player = 1 ;
-        resetBtn.style.display = 'inline';
-        winner.textContent = `player ${player + 1} won!`;
+        winner.textContent = `Os Have Won!`;
+        resetBtn.style.display = "inline";
+        clearMemory();
+    }
+    else if(checkWin(arrX)) {
+        for(const cell of cells) cell.classList.add('selected');
+        winner.textContent = "Xs Have Won!"
+        resetBtn.style.display = "inline";
+        clearMemory();
     }
     //checks if no one won
     else if(arrX.length + arrO.length === 9) {
         resetBtn.style.display = 'inline';
         winner.textContent = 'Draw!';
+        clearMemory();
     }
 }
 function updateGameDisplay() {
     selectorContainer.style.display = "none";
+    selectPlayerContainer.style.display = "none";
     gameContainer.style.display = "inline";
 }
 function updateToStart() {
     selectorContainer.style.display = "inline";
     gameContainer.style.display = "none";
 }
+function setPlayer0() {
+    player = 0;
+    updateGameDisplay();
+}
+function setPlayer1() {
+    player = 1;
+    updateGameDisplay();
+}
+function setPlayer() {
+    player = 0;
+    selectPlayerOrPcContainer.style.display = "none";
+    updateGameDisplay();
+}
 
 //sets the game to a 1V1 game
 function set1V1() {
-    updateGameDisplay();
-    for(let i=0;i<cells.length; i++){
+    selectPlayerContainer.style.display = "inline";
+    selectorContainer.style.display = "none";
+    btnPlayer0.addEventListener('click', setPlayer0);
+    btnPlayer1.addEventListener('click', setPlayer1);
+    for(let i=0;i<cells.length;i++) {
         const currentCell = cells[i];
-        const gameEvents = function() {
-            //switches the player and marks the cell as X or O
-            switchingPlayer(currentCell);
-            //pushes the Xs to the memoryX and the Os to memory
-            pushToArr(currentCell, i);
-            //makes arrays from the objects
-            const arrX = Object.keys(memoryX);
-            const arrO = Object.keys(memoryO);
-            //checks if someone won and if so, declares the winner
-            declareWinner(arrX, arrO);
+        const gameEvents = function (){
+        switchingPlayer(currentCell);
+        pushToArr(currentCell, i);
+        const arrX = Object.keys(memoryX);
+        const arrO = Object.keys(memoryO); 
+        declareWinner(arrX, arrO);
         }
-        //every time the player clicks it calls the gameEvents function
         currentCell.addEventListener('click', gameEvents);
     }
 }
 
-function setEasy() {
+const randomNum = () => Math.trunc(Math.random() * 9);
 
+function createRandomCell() {
+    let random = randomNum();
+    let randomCell = cells[random];
+    if(randomCell.classList.contains('selected')){
+        random = randomNum();
+        randomCell = cells[random];
+    }
+    return randomCell;
+}
+function getIndex(randomCell) {
+    for(let i=0;i<cells.length;i++){
+        if(cells[i] === randomCell) return i;
+    }
+}
+
+function setEasy() {
+    selectPlayerOrPcContainer.style.display = "inline";
+    selectorContainer.style.display = "none";
+    btnPlayer.addEventListener('click', setPlayer);
+    for(let i=0;i<cells.length;i++) {
+        const currentCell = cells[i];
+        function gameEvents() {
+            switchingPlayer(currentCell);
+            const randomCell = createRandomCell();
+            switchingPlayer(randomCell);
+            const randomIndex = getIndex(randomCell);
+            pushToArr(currentCell, i);
+            pushToArr(randomCell,randomIndex);
+            const arrX = Object.keys(memoryX);
+            const arrO = Object.keys(memoryO);
+            declareWinner(arrX,arrO);
+        }
+        currentCell.addEventListener('click',gameEvents);
+    }
 }
 function setHard() {
 
 }
+
 
 //reset button
 resetBtn.addEventListener('click', resetAll);
